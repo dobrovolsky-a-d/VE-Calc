582d344236311c8d4953e62e29c7f1166a76b336
dobrovolsky-a-d <65624046+dobrovolsky-a-d@users.noreply.github.com>
2025-10-11 23:54:32 +0800

    Update exportRomRaider.js

--- exportRomRaider.js
+++ exportRomRaider.js
@@ -1,7 +1,6 @@
 export function exportRomRaider(matrix) {
   let csv = matrix.map(r => r.map(v => v.toFixed(2)).join(',')).join('\n');
   const blob = new Blob([csv], { type: 'text/csv' });
-
   const a = document.createElement('a');
   a.href = URL.createObjectURL(blob);
   a.download = 'VE_new.csv';
