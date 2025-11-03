2f6cb25fbf5e25712e270bc46de987f7c349387b
dobrovolsky-a-d <65624046+dobrovolsky-a-d@users.noreply.github.com>
2025-11-03 09:52:17 +0300

    Delete ve-calculator_v1.1 directory

--- ve-calculator_v1.1/exportRomRaider.js
+++
@@ -1,8 +0,0 @@
-export function exportRomRaider(matrix){
-  const csv = matrix.map(r => r.map(v => Number.isFinite(v)?v.toFixed(2):'').join(',')).join('\n');
-  const blob = new Blob([csv], {type:'text/csv'});
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = 'VE_new.csv';
-  a.click();
-}
