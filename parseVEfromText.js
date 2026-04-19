f5be939482e406a612c2c9c6f25d310bcda7edda
dobrovolsky-a-d <65624046+dobrovolsky-a-d@users.noreply.github.com>
2026-04-19 09:14:18 +0300

    Update parseVEfromText.js

--- parseVEfromText.js
+++ parseVEfromText.js
@@ -1,31 +1,10 @@
 export function parseVEFromText(rpmText, mapText, veText) {
 
-  const rpmAxis = rpmText
-    .split(/[\s,]+/)
-    .map(Number)
-    .filter(v => !isNaN(v));
+  const rpmAxis = rpmText.split(/[\s,]+/).map(Number).filter(v => !isNaN(v));
+  const loadAxis = mapText.split(/[\s,]+/).map(Number).filter(v => !isNaN(v));
 
-  const loadAxis = mapText
-    .split(/[\s,]+/)
-    .map(Number)
-    .filter(v => !isNaN(v));
-
-  const rows = veText
-    .trim()
-    .split("\n")
-    .map(line =>
-      line.trim().split(/[\s,]+/).map(Number)
-    );
-
-  if (rows.length !== rpmAxis.length) {
-    throw new Error("Rows != RPM axis");
-  }
-
-  for (let r of rows) {
-    if (r.length !== loadAxis.length) {
-      throw new Error("Cols != MAP axis");
-    }
-  }
+  const rows = veText.trim().split("\n")
+    .map(l => l.trim().split(/[\s,]+/).map(Number));
 
   return {
     rows: rows.length,
